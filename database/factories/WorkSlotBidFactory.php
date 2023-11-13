f5ce4f39590d96665982ff6f323cd4b2b04b40d3
joshuacjj7 <joshua.chng7@gmail.com>
2023-11-13 15:32:36 +0800

    Full working Factories with no conflicts

--- database/factories/WorkSlotBidFactory.php
+++ database/factories/WorkSlotBidFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use App\Models\WorkSlot;
+use App\Models\WorkSlotBid;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
@@ -13,14 +14,35 @@ class WorkSlotBidFactory extends Factory
 
     public function definition()
     {
-        $user=User::all()->where('staff_role_id','!=',null)->random();
-        $workslotId = Workslot::query()
-        ->where('staff_role_id',$user->staff_role_id)
-        ->get()
-        ->random();
+        //get random user
+        $user=User::all()
+                ->where('staff_role_id','!=',null)
+                ->random();
+        //get non-Rejected existingBids from random user 
+        $existingBid = WorkSlotBid::query()
+                    ->where('user_id', $user->id)
+                    ->where('status', '>=', '0')
+                    ->get();
+
+        do{
+            //get random Workslot
+            $workslot = Workslot::query()
+            ->where('staff_role_id',$user->staff_role_id)
+            ->get()
+            ->random();
+
+            //add in to check workslotid
+            $existingBid = $existingBid
+                            ->where('work_slot_id', $workslot->id)
+                            ->where('user_id', $user->id)
+                            ->where('status', '>=', '0')
+                            ->first();
+
+        }while($existingBid);
+
         return [
             'user_id' => $user->id,
-            'work_slot_id' => $workslotId,
+            'work_slot_id' => $workslot->id,
             'status' =>$this->faker->numberBetween(-1,3),
         ];
     }
