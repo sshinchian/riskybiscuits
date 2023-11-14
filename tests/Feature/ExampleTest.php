09896cc8bd8a4e7687be727ca4c9d321b2631012
racezx812 <thisura812_new@yahoo.com>
2023-11-15 07:30:11 +0800

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 
 class UserTest extends TestCase
@@ -12,9 +13,15 @@ class UserTest extends TestCase
     use RefreshDatabase;
     public function test_login_redirect_to_dashboard_successfully() {
 
+        User::factory()->create([
+            "email"=> "superadmin@gmail.com",
+            "password"=> bcrypt("password"),
+        ]);
+
+
        $response = $this->post('/login',[
         'email'=> 'superadmin@gmail.com',
-        'password' => '111'
+        'password' => 'password'
        ]);
 
        $response->assertStatus(302);
