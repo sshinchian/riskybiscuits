84a9cf9497bea0e94a05efe85c8554efc64b4a50
racezx812 <thisura812_new@yahoo.com>
2023-11-15 07:34:00 +0800

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 use App\Models\User;
 
@@ -14,8 +15,16 @@ class UserTest extends TestCase
     public function test_login_redirect_to_dashboard_successfully() {
 
         User::factory()->create([
-            "email"=> "superadmin@gmail.com",
-            "password"=> bcrypt("password"),
+
+            'first_name'    => 'Super',
+            'last_name'     => 'Admin',
+            'email'         =>  'superadmin@gmail.com',
+            'mobile_number' =>  '81818181',
+            'password'      =>  Hash::make('111'),
+            'role_id'       => 1
+
+            /* "email"=> "superadmin@gmail.com",
+             "password"=> bcrypt("password"), */
         ]);
 
 
